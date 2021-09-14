20f0f64d512365cc82492cff3d37796143fa2dde
GitHub Action <action@github.com>
2021-09-14 13:59:52 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/merlinmaretaes/testtemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.merlinmaretaes.testtemplate.services
+
+import com.github.merlinmaretaes.testtemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
